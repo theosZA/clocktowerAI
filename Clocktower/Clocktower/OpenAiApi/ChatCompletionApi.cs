ce3e73c540ec2c0e2309a6e71db13fe8229b2e65
theosza <theosZA+github@googlemail.com>
2023-12-26 15:55:07 +0200

    Retry API requests with exponential backoff

--- Clocktower/Clocktower/OpenAiApi/ChatCompletionApi.cs
+++ Clocktower/Clocktower/OpenAiApi/ChatCompletionApi.cs
@@ -1,4 +1,6 @@
 ﻿using Clocktower.OpenAiApi.Model;
+using Polly;
+using Polly.Retry;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json;
@@ -19,12 +21,31 @@ namespace Clocktower.OpenAiApi
 
         public async Task<string> RequestChatCompletion(IEnumerable<(Role role, string message)> messages)
         {
-            using var response = await httpClient.PostAsJsonAsync("chat/completions", BuildChatCompletionRequest(messages), jsonSerializerOptions);
-            response.EnsureSuccessStatusCode();
+            var request = BuildChatCompletionRequest(messages);
+            using var response = await RequestChatCompletion(request);
             var chatResponse = await response.Content.ReadFromJsonAsync<ChatCompletionResponse>() ?? throw new Exception("No chat completion received from Open API");
             var usage = chatResponse.Usage;
             tokenCounter.NewTokenUsage(usage.PromptTokens, usage.CompletionTokens, usage.TotalTokens);
             return chatResponse.Choices.First().Message.Content;
+        }
+
+        private static async Task<HttpResponseMessage> RequestChatCompletion(ChatCompletionRequest request)
+        {
+            return await policy.ExecuteAsync(async () =>
+            {
+                HttpResponseMessage? response = null;
+                try
+                {
+                    response = await httpClient.PostAsJsonAsync("chat/completions", request, jsonSerializerOptions);
+                    response.EnsureSuccessStatusCode();
+                    return response;
+                }
+                catch (Exception)
+                {
+                    response?.Dispose();
+                    throw;
+                }
+            });
         }
 
         private static ChatCompletionRequest BuildChatCompletionRequest(IEnumerable<(Role role, string message)> messages)
@@ -51,6 +72,10 @@ namespace Clocktower.OpenAiApi
         {
             BaseAddress = new Uri("https://api.openai.com/v1/")
         };
+
+        private static readonly AsyncRetryPolicy policy = Policy.Handle<HttpRequestException>()
+                                                                .WaitAndRetryAsync(retryCount: 9,
+                                                                                   sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))); // Exponential backoff formula
 
         private static readonly JsonSerializerOptions jsonSerializerOptions = new()
         {
