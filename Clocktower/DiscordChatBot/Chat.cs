bf7f243259f8b029889abbf6e310a9efff67fa37
theosza <theosZA+github@googlemail.com>
2024-07-05 08:49:05 +0200

    Basic Discord chat client

---
+++ Clocktower/DiscordChatBot/Chat.cs
@@ -0,0 +1,50 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace DiscordChatBot
+{
+    /// <summary>
+    /// Represents a chat between the bot and an individual. We can send and receive messages on this chat.
+    /// </summary>
+    public class Chat
+    {
+        public Chat(string name)
+        {
+            this.name = name;
+        }
+
+        public async Task Create(SocketGuild guild)
+        {
+            channel = guild.TextChannels.FirstOrDefault(channel => channel.Name == name);
+            channel ??= await guild.CreateTextChannelAsync(name);
+        }
+
+        public async Task SendMessage(string message)
+        {
+            if (channel != null)
+            {
+                await channel.SendMessageAsync(message);
+            }
+        }
+
+        public void MessageReceived(string message)
+        {
+            lastReceivedMessage = message;
+            messageReceivedTsc?.TrySetResult();
+        }
+
+        public async Task<string> SendMessageAndGetResponse(string messageToSend)
+        {
+            messageReceivedTsc = new TaskCompletionSource();
+            await SendMessage(messageToSend);
+            await messageReceivedTsc.Task;
+            return lastReceivedMessage ?? string.Empty;
+        }
+
+        private readonly string name;
+        private ITextChannel? channel;
+
+        private string? lastReceivedMessage;
+        private TaskCompletionSource? messageReceivedTsc;
+    }
+}
---
+++ Clocktower/DiscordChatBot/ChatClient.cs
@@ -0,0 +1,83 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System.Diagnostics;
+
+namespace DiscordChatBot
+{
+    /// <summary>
+    /// The ChatClient wraps all <see cref="Chat"/> instances.
+    /// </summary>
+    /// <remarks>Currently this client is limited to a single guild named "ClocktowerVsAI".</remarks>
+    public class ChatClient
+    {
+        public const string GuildName = "ClocktowerVsAI";
+
+        public async Task Start()
+        {
+            client.Log += Log;
+            client.Ready += Ready;
+            client.MessageReceived += MessageReceived;
+
+            var token = Environment.GetEnvironmentVariable("DISCORD_BOT_TOKEN", EnvironmentVariableTarget.User);
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+            await readyTsc.Task;
+        }
+
+        public async Task<Chat> CreateChat(string name)
+        {
+            var guild = client.Guilds.FirstOrDefault(guild => guild.Name == GuildName) ?? throw new Exception($"Not a member of guild {GuildName}");
+            var chat = new Chat(name);
+            chats.Add(name, chat);
+            await chat.Create(guild);
+            return chat;
+        }
+
+        private static Task Log(LogMessage logMessage)
+        {
+            Debug.WriteLine(logMessage.ToString());
+            return Task.CompletedTask;
+        }
+
+        private Task Ready()
+        {
+            var guilds = client.Guilds.ToList();
+            Debug.WriteLine($"Guilds: {string.Join(", ", guilds.Select(guild => guild.Name))}");
+            if (guilds.Count > 0)
+            {
+                var guild = guilds[0];
+                var channels = guild.TextChannels.ToList();
+                Debug.WriteLine($"Channels: {string.Join(", ", channels.Select(channel => channel.Name))}");
+            }
+            readyTsc.TrySetResult();
+
+            return Task.CompletedTask;
+        }
+
+        private Task MessageReceived(SocketMessage message)
+        {
+            Debug.WriteLine($"Message: {message.Channel.Name} - {message.Author.GlobalName} - {message.Content}");
+
+            if (!message.Author.IsBot && chats.TryGetValue(message.Channel.Name, out var chat))
+            {
+                chat.MessageReceived(message.Content);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private static DiscordSocketClient BuildClient()
+        {
+            var config = new DiscordSocketConfig
+            {
+                GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+            };
+            return new DiscordSocketClient(config);
+        }
+
+        private readonly DiscordSocketClient client = BuildClient();
+        private readonly TaskCompletionSource readyTsc = new();
+
+        private readonly Dictionary<string, Chat> chats = new();
+    }
+}
