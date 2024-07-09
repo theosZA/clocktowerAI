d3231595d9b2c286577a670be22f0794c3fd8546
theosza <theosZA+github@googlemail.com>
2024-07-09 17:18:36 +0200

    Test for Soldier

---
+++ Clocktower/ClocktowerScenarioTests/Tests/SoldierTests.cs
@@ -0,0 +1,25 @@
+using Clocktower.Game;
+using ClocktowerScenarioTests.Mocks;
+
+namespace ClocktowerScenarioTests.Tests
+{
+    public class SoldierTests
+    {
+        [Test]
+        public async Task Soldier_SafeFromImp()
+        {
+            // Arrange
+            var (setup, game) = ClocktowerGameBuilder.BuildDefault("Imp,Soldier,Ravenkeeper,Saint,Baron,Slayer,Fisherman,Mayor");
+
+            setup.Agents[0].MockImp(Character.Soldier);
+
+            // Act
+            await game.StartGame();
+            await game.RunNightAndDay();
+            await game.RunNightAndDay();
+
+            // Assert
+            await setup.Agents[1].DidNotReceive().YouAreDead();
+        }
+    }
+}
