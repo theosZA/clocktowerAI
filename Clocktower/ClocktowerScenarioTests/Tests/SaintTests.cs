12a2b1bf7c31abd159fef7f04d072c995b41ce90
theosza <theosZA+github@googlemail.com>
2024-07-11 22:59:48 +0200

    Tests for Saint

---
+++ Clocktower/ClocktowerScenarioTests/Tests/SaintTests.cs
@@ -0,0 +1,57 @@
+using Clocktower.Game;
+using ClocktowerScenarioTests.Mocks;
+
+namespace ClocktowerScenarioTests.Tests
+{
+    public class SaintTests
+    {
+        [Test]
+        public async Task Saint_LoseIfExecuted()
+        {
+            // Arrange
+            var (setup, game) = ClocktowerGameBuilder.BuildDefault("Imp,Soldier,Ravenkeeper,Saint,Baron,Fisherman,Mayor");
+            setup.Agents[0].MockNomination(Character.Saint);
+
+            // Act
+            await game.StartGame();
+            await game.RunNightAndDay();
+
+            // Assert
+            Assert.That(game.Finished, Is.True);
+            Assert.That(game.Winner, Is.EqualTo(Alignment.Evil));
+        }
+
+        [Test]
+        public async Task Saint_FineIfKilledByImp()
+        {
+            // Arrange
+            var (setup, game) = ClocktowerGameBuilder.BuildDefault("Imp,Soldier,Ravenkeeper,Saint,Baron,Fisherman,Mayor");
+            setup.Agent(Character.Imp).MockImp(Character.Saint);
+
+            // Act
+            await game.StartGame();
+            await game.RunNightAndDay();
+            await game.RunNightAndDay();
+
+            // Assert
+            Assert.That(game.Finished, Is.False);
+        }
+
+        [Test]
+        public async Task Saint_FineIfKilledByAssassin()
+        {
+            // Arrange
+            var (setup, game) = ClocktowerGameBuilder.BuildDefault("Imp,Soldier,Ravenkeeper,Saint,Assassin,Fisherman,Mayor");
+            setup.Agent(Character.Imp).MockImp(Character.Soldier);
+            setup.Agent(Character.Assassin).MockAssassin(Character.Saint);
+
+            // Act
+            await game.StartGame();
+            await game.RunNightAndDay();
+            await game.RunNightAndDay();
+
+            // Assert
+            Assert.That(game.Finished, Is.False);
+        }
+    }
+}
